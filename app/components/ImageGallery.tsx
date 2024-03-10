cc563c48210a14a205b480bd083b5429ec19652a
Jatin Goyal <25JG2001@GMAIL.COM>
2024-03-10 20:00:59 +0530

    image zoom duration

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -34,7 +34,7 @@ export default function ImageGallery({ images }: iAppProps) {
         ))}
       </div>
 
-      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4 h-full lg:h-4/5 hover:origin-top lg:hover:scale-150  transition duration-150 ease-out hover:ease-in-out duration-300">
+      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4 h-full lg:h-4/5 hover:origin-top lg:hover:scale-150  transition duration-150 ease-out hover:ease-in-out duration-500">
         <Image
           src={urlFor(bigImage).url()}
           alt="Alphabet visor glass, alphhabet visor glass, 2-wheeler, two-wheeler, 2 wheeler, two wheeler, visor, visor glass, windshield, wind sheild, fairing, fender, auto part, plastic parts, manufactururer, bawana, delhi, india, Alphabet visor glass hero,hero visor glass, honda visor glass, honda, tvs visor glass, tvs, yamaha visor glass, yamaha, bajaj visor glass, bajaj, mahindra visor glass, mahindra, suzuki visor glass, Suzuki, ktm, bawana, delhi, mumbai, pune, panjab, jharkhand, up, kanpur, lucknow"
